37b9baeedfc9e7d201bceb53fff86e2ddc28abe7
Dan Audne <dan@audne.com>
2023-05-08 21:40:03 +0200

    remove debug app

--- cmd/debug/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	tnp "github.com/ProfChaos/torrent-name-parser"
-)
-
-func main() {
-	tnp.DebugParser("The.Wizard.of.Oz.1939.4K.HDR.DV.2160p.BDRemux Ita Eng x265-NAHOM")
-}
